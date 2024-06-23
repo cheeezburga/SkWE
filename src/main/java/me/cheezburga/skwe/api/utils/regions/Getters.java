bcc1f13dc9ba4f12f6b1cf34163bbce06adc4459
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-06-23 19:21:53 +1000

    Adds static region methods - Adds a method to get each kind of supported region

---
+++ src/main/java/me/cheezburga/skwe/api/utils/regions/Getters.java
@@ -0,0 +1,35 @@
+package me.cheezburga.skwe.api.utils.regions;
+
+import com.fastasyncworldedit.core.regions.PolyhedralRegion;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.Vector2;
+import com.sk89q.worldedit.math.Vector3;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import com.sk89q.worldedit.regions.CylinderRegion;
+import com.sk89q.worldedit.regions.EllipsoidRegion;
+import me.cheezburga.skwe.api.utils.Utils;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+public class Getters {
+
+    public static CuboidRegion getCuboidRegion(Location loc1, Location loc2, World world) {
+        return new CuboidRegion(BukkitAdapter.adapt(world), Utils.blockVector3From(loc1), Utils.blockVector3From(loc2));
+    }
+
+    public static CylinderRegion getCylinderRegion(Location center, double radiusX, double radiusZ, int height) {
+        return new CylinderRegion(BukkitAdapter.adapt(center.getWorld()), Utils.blockVector3From(center), Vector2.at(radiusX, radiusZ), center.getBlockY(), center.getBlockY() + height);
+    }
+
+    public static EllipsoidRegion getEllipsoidRegion(Location center, double radiusX, double radiusY, double radiusZ) {
+        return new EllipsoidRegion(BukkitAdapter.adapt(center.getWorld()), Utils.blockVector3From(center), Vector3.at(radiusX, radiusY, radiusZ));
+    }
+
+    public static PolyhedralRegion getPolyRegion(Location... vertices) {
+        PolyhedralRegion region = new PolyhedralRegion(BukkitAdapter.adapt(vertices[0].getWorld()));
+        for (Location loc : vertices) {
+            region.addVertex(Utils.blockVector3From(loc));
+        }
+        return region;
+    }
+}
