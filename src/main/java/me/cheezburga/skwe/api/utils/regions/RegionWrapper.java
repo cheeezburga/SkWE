2978982068d1e261062d9e633a96d76887721e0b
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-06-23 23:52:19 +1000

    Added toString to RegionWrapper - Will need to come back to this and modify the toVariableString method - See TODOs

--- src/main/java/me/cheezburga/skwe/api/utils/regions/RegionWrapper.java
+++ src/main/java/me/cheezburga/skwe/api/utils/regions/RegionWrapper.java
@@ -1,12 +1,17 @@
 package me.cheezburga.skwe.api.utils.regions;
 
+import com.fastasyncworldedit.core.regions.PolyhedralRegion;
+import com.sk89q.worldedit.regions.ConvexPolyhedralRegion;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import com.sk89q.worldedit.regions.CylinderRegion;
+import com.sk89q.worldedit.regions.EllipsoidRegion;
 import com.sk89q.worldedit.regions.Region;
 import org.bukkit.World;
 
 public class RegionWrapper {
 
-    private Region region;
-    private World world;
+    final private Region region;
+    final private World world;
 
     public RegionWrapper(Region region, World world) {
         this.region = region;
@@ -21,4 +26,29 @@ public class RegionWrapper {
         return this.region;
     }
 
+    private String getRegionType() {
+        if (this.region instanceof CuboidRegion) {
+            return "cuboid";
+        } else if (this.region instanceof CylinderRegion) {
+            return "cylinder";
+        } else if (this.region instanceof EllipsoidRegion) {
+            return "ellipsoid";
+        } else if (this.region instanceof PolyhedralRegion) {
+            return "polyhedral";
+        } else if (this.region instanceof ConvexPolyhedralRegion) {
+            return "convex polyhedral";
+        }
+        return "unknown";
+    }
+
+    @Override
+    public String toString() {
+        // TODO: decide whether this should include coords of associated points - might get long for poly/convex
+        return getRegionType() + " region in world " + this.world.getName();
+    }
+
+    public String toVariableString() {
+        // TODO: change this to actually return a proper variable string
+        return toString();
+    }
 }
