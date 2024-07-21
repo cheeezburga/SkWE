17f757755412c42c5d9f25ddf1695315ba1a54c9
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-07-22 01:27:53 +1000

    Updates PATTERN_PREFIX constant to include some FAWE patterns

--- src/main/java/me/cheezburga/skwe/api/utils/Utils.java
+++ src/main/java/me/cheezburga/skwe/api/utils/Utils.java
@@ -25,7 +25,7 @@ public class Utils {
 
     @SuppressWarnings("deprecation")
     public static final String PLUGIN_PREFIX = ChatColor.of("#00FFFF") + "[skript-worldedit]" + ChatColor.RESET;
-    public static final String PATTERN_PREFIX = "(use (world[ ]edit|we) to|world[ ]edit|we)";
+    public static final String PATTERN_PREFIX = "(use ([fastasync]world[ ]edit|[fa]we) to|[fastasync]world[ ]edit|[fa]we)";
     private static final java.util.regex.Pattern HEX_PATTERN = java.util.regex.Pattern.compile("<#([A-Fa-f\\d]){6}>");
     private static final boolean SKRIPT_EXISTS = Bukkit.getPluginManager().getPlugin("Skript") != null;
 
