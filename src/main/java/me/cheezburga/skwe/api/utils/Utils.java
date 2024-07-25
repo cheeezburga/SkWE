d490465a016e61788220259d3aee4b842358c054
cheeezburga <47320303+cheeezburga@users.noreply.github.com>
2024-07-26 02:04:05 +1000

    Changes and adds some BlockVector3 util methods

--- src/main/java/me/cheezburga/skwe/api/utils/Utils.java
+++ src/main/java/me/cheezburga/skwe/api/utils/Utils.java
@@ -19,7 +19,10 @@ import org.bukkit.World;
 import org.bukkit.inventory.meta.BlockDataMeta;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
+import java.util.stream.Collectors;
 
 public class Utils {
 
@@ -43,8 +46,16 @@ public class Utils {
      * @param location  input Location
      * @return          WorldEdit BlockVector3
      */
-    public static BlockVector3 blockVector3From(Location location) {
+    public static BlockVector3 toBlockVector3(Location location) {
         return BlockVector3.at(location.getX(), location.getY(), location.getZ());
+    }
+
+    public static List<BlockVector3> toBlockVector3List(List<Location> locations) {
+        return locations.stream().map(Utils::toBlockVector3).collect(Collectors.toList());
+    }
+
+    public static BlockVector3[] toBlockVector3Array(Location[] locations) {
+        return Arrays.stream(locations).map(Utils::toBlockVector3).toArray(BlockVector3[]::new);
     }
 
     /**
